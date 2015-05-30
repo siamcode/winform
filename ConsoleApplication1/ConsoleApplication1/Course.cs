c432f7105f8ba0e0cbc7087b47691ee709ab20dd
Somchai Banmai <somchai.banmai@gmail.com>
2015-05-31 02:02:05 +0700

    forget classes

---
+++ ConsoleApplication1/ConsoleApplication1/Course.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    public class Course
+    {
+        private int id;
+        private string name;
+
+        public Course(int id, string name)
+        {
+            this.id = id;
+            this.name = name;
+        }
+
+        public int ID { get; set; }
+        public string NAME { get; set; }
+    }
+}
